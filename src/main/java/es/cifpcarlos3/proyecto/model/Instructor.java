043a8ce83f21b73950e0df92d9198f0791926d48
Juan Francisco Mateo <jf.profesional.ai@gmail.com>
2026-05-29 18:40:53 +0200

    FIX: Modelos - añadido campo tipo a Inmersion, idCliente a Cliente, @EqualsAndHashCode(callSuper=true)

--- src/main/java/es/cifpcarlos3/proyecto/model/Instructor.java
+++ src/main/java/es/cifpcarlos3/proyecto/model/Instructor.java
@@ -3,13 +3,13 @@ package es.cifpcarlos3.proyecto.model;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
-
+@EqualsAndHashCode(callSuper = true)
 public class Instructor extends Persona{
     private int idInstructor;
     private Certificacion certificacion;
