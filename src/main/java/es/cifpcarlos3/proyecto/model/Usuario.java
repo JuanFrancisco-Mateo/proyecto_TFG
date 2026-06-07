428dc006bc6aee6c985e01e23bdd67c2a4d47b97
Irene Martinez Diaz <1548366@alu.murciaeduca.es>
2026-06-07 22:11:38 +0200

    Corregido error atributos duplicados

--- src/main/java/es/cifpcarlos3/proyecto/model/Usuario.java
+++ src/main/java/es/cifpcarlos3/proyecto/model/Usuario.java
@@ -1,6 +1,7 @@
 package es.cifpcarlos3.proyecto.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -8,20 +9,15 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
-
+@EqualsAndHashCode(callSuper = true)
 public class Usuario extends Persona{
     private int idUsuario;
-    private String nombre;
-    private String apellidos;
-    private String email;
-    private String telefono;
     private String username;
     private String passwordHash; //Se guarda el hash de la contraseña, no la contraseña real
     private Rol rol;
 
     @Override
     public String toString(){
-        return nombre+ " "+ apellidos;
+        return nombre+ " "+ apellidos+" - " +dni;
     }
 }
